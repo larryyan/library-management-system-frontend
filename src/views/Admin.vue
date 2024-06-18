a8ebd1eb7e3163394efcdbfd9623da1637d85647
严嘉哲 <2955975006@qq.com>
2024-06-18 23:06:00 +0800

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,14 +3,17 @@
         <div class="sidebar">
             <h2>操作选项</h2>
             <ul>
-                <li :class="{ active: currentTab === 'add' }" @click="currentTab = 'add'">图书增加</li>
-                <li :class="{ active: currentTab === 'delete' }" @click="currentTab = 'delete'">图书删除</li>
-                <li :class="{ active: currentTab === 'put' }" @click="currentTab = 'put'">图书修改</li>
+                <li :class="{ active: currentTab === 'add-books' }" @click="currentTab = 'add-books'">图书入库</li>
+                <li :class="{ active: currentTab === 'delete-books' }" @click="currentTab = 'delete-books'">图书删除</li>
+                <li :class="{ active: currentTab === 'put-books' }" @click="currentTab = 'put-books'">图书位置修改</li>
+                <li :class="{ active: currentTab === 'add-book-info' }" @click="currentTab = 'add-book-info'">图书信息录入</li>
+                <li :class="{ active: currentTab === 'delete-book-info' }" @click="currentTab = 'delete-book-info'">图书信息删除</li>
+                <li :class="{ active: currentTab === 'put-book-info' }" @click="currentTab = 'put-book-info'">图书信息修改</li>
             </ul>
         </div>
         <div class="content">
-            <div v-show="currentTab === 'add'" class="tab-content">
-                <h2>图书增加</h2>
+            <div v-show="currentTab === 'add-books'" class="tab-content">
+                <h2>馆藏书籍增加</h2>
                 <form @submit.prevent="addBook">
                     <div v-for="(value, key) in bookFields" :key="key" class="form-group">
                         <label :for="key">{{ value.label }}</label>
@@ -19,22 +22,56 @@
                     <button type="submit" class="btn-submit">添加图书</button>
                 </form>
             </div>
-            <div v-show="currentTab === 'delete'" class="tab-content">
-                <h2>图书删除</h2>
+            <div v-show="currentTab === 'delete-books'" class="tab-content">
+                <h2>馆藏书籍删除</h2>
                 <form @submit.prevent="deleteBook">
                     <div class="form-group">
-                        <label for="deleteIsbn">ISBN：</label>
-                        <input type="text" id="deleteIsbn" v-model="isbn" required>
+                        <label for="deleteIsbn">书号：</label>
+                        <input type="text" id="deleteIsbn" v-model="book_id" required>
                     </div>
                     <button type="submit" class="btn-submit">删除图书</button>
                 </form>
             </div>
-            <div v-show="currentTab === 'put'" class="tab-content">
-                <h2>图书修改</h2>
+            <div v-show="currentTab === 'put-books'" class="tab-content">
+                <h2>馆藏书籍修改</h2>
                 <form @submit.prevent="updateBook">
+                    <div class="form-group">
+                        <label for="deleteIsbn">书号：</label>
+                        <input type="text" id="deleteIsbn" v-model="book_id" required>
+                    </div>
                     <div v-for="(value, key) in bookFields" :key="key" class="form-group">
                         <label :for="key">{{ value.label }}</label>
                         <input type="text" :id="key" v-model="book[key]" required>
+                    </div>
+                    <button type="submit" class="btn-submit">修改图书</button>
+                </form>
+            </div>
+            <div v-show="currentTab === 'add-book-info'" class="tab-content">
+                <h2>图书信息录入</h2>
+                <form @submit.prevent="addBookInfo">
+                    <div v-for="(value, key) in book_info_fields" :key="key" class="form-group">
+                        <label :for="key">{{ value.label }}</label>
+                        <input type="text" :id="key" v-model="book_info[key]" required>
+                    </div>
+                    <button type="submit" class="btn-submit">图书信息录入</button>
+                </form>
+            </div>
+            <div v-show="currentTab === 'delete-book-info'" class="tab-content">
+                <h2>图书信息删除</h2>
+                <form @submit.prevent="deleteBookInfo">
+                    <div class="form-group">
+                        <label for="deleteIsbn">ISBN：</label>
+                        <input type="text" id="deleteIsbn" v-model="isbn" required>
+                    </div>
+                    <button type="submit" class="btn-submit">图书信息删除</button>
+                </form>
+            </div>
+            <div v-show="currentTab === 'put-book-info'" class="tab-content">
+                <h2>图书信息修改</h2>
+                <form @submit.prevent="updateBookInfo">
+                    <div v-for="(value, key) in book_info_fields" :key="key" class="form-group">
+                        <label :for="key">{{ value.label }}</label>
+                        <input type="text" :id="key" v-model="book_info[key]" required>
                     </div>
                     <button type="submit" class="btn-submit">修改图书</button>
                 </form>
@@ -50,23 +87,30 @@ export default {
     name: 'Admin',
     data() {
         return {
-            currentTab: 'add',
+            currentTab: 'add-books',
             book: {
+                isbn: '',
+                book_address: '',
+            },
+            book_info: {
                 isbn: '',
                 book_title: '',
                 book_author: '',
                 book_publisher: '',
                 book_type: '',
-                book_address: '',
             },
             isbn: '',
+            book_id: '',
             bookFields: {
+                isbn: { label: 'ISBN：' },
+                book_address: { label: '图书位置：' },
+            },
+            book_info_fields: {
                 isbn: { label: 'ISBN：' },
                 book_title: { label: '书名：' },
                 book_author: { label: '作者：' },
                 book_publisher: { label: '出版社：' },
                 book_type: { label: '图书类型：' },
-                book_address: { label: '图书位置：' },
             },
         };
     },
@@ -83,7 +127,7 @@ export default {
                 });
         },
         deleteBook() {
-            axios.delete('http://127.0.0.1:5000/book/${this.isbn}')
+            axios.delete(`http://127.0.0.1:5000/book/${this.book_id}`)
                 .then(response => {
                     console.log('删除图书成功:', response.data);
                     alert('删除图书成功');
@@ -94,8 +138,41 @@ export default {
                 });
         },
         updateBook() {
-            const { isbn, ...bookData } = this.book;
-            axios.put(`http://127.0.0.1:5000/book/${isbn}`, bookData)
+            axios.put(`http://127.0.0.1:5000/book/${this.book_id}`, bookData)
+                .then(response => {
+                    console.log('更新图书成功:', response.data);
+                    alert('更新图书成功');
+                })
+                .catch(error => {
+                    console.error('更新图书失败:', error);
+                    alert('更新图书失败');
+                });
+        },
+        addBookInfo() {
+            axios.post('http://127.0.0.1:5000/book_info/', this.book_info)
+                .then(response => {
+                    console.log('添加图书成功:', response.data);
+                    alert('添加图书成功');
+                })
+                .catch(error => {
+                    console.error('添加图书失败:', error);
+                    alert('添加图书失败');
+                });
+        },
+        deleteBookInfo() {
+            axios.delete(`http://127.0.0.1:5000/book_info/${this.isbn}`)
+                .then(response => {
+                    console.log('删除图书成功:', response.data);
+                    alert('删除图书成功');
+                })
+                .catch(error => {
+                    console.error('删除图书失败:', error);
+                    alert('删除图书失败');
+                });
+        },
+        updateBookInfo() {
+            const { isbn, ...bookData } = this.book_info;
+            axios.put(`http://127.0.0.1:5000/book_info/${isbn}`, bookData)
                 .then(response => {
                     console.log('更新图书成功:', response.data);
                     alert('更新图书成功');
